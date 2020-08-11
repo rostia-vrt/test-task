ac0634fd7f4ae9274fca470ef21c8b0195859a6d
rostia-vrt <vrostyk07@gmail.com>
2020-08-11 12:44:22 +0300

    Updates

---
+++ precache-manifest.c521bdadb853c22ded477d7d95882821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b851b759a21d8eef8b3cbf5987837f93",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "031515229cfc27134c37",
+    "url": "/test-task/static/css/main.e43a1c31.chunk.css"
+  },
+  {
+    "revision": "62a3b49de55d9f3c826a",
+    "url": "/test-task/static/js/2.9c1b31fc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/test-task/static/js/2.9c1b31fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "031515229cfc27134c37",
+    "url": "/test-task/static/js/main.bf03bc4d.chunk.js"
+  },
+  {
+    "revision": "79892429a743b8954c05",
+    "url": "/test-task/static/js/runtime-main.4af6997d.js"
+  }
+]);
